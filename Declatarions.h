f8da0a6d806a861c321c06a434d3327047b06b62
Garo <45628584+Garo2@users.noreply.github.com>
2020-06-11 03:52:52 +0200

    Added extra functions

--- Declatarions.h
+++ Declatarions.h
@@ -1,12 +1,10 @@
-/* Declartions.h
+/* mipslab.h
    Header file for all labs.
    This file written 2015 by F Lundevall
    Some parts are original code written by Axel Isaksson
-   Modified by Garo Malko
-   Modified by Karoline Malko
 
-   Latest update 2020-03-01 by Garo Malko
-   
+   Latest update 2015-08-28 by F Lundevall
+
    For copyright and licensing, see file COPYING */
 
 /* Declare display-related functions from mipslabfunc.c */
@@ -60,5 +58,10 @@ int motionSensorOn();
 int switch2Page1();
 int checkPage ();
 int switch2Page2();
-int resetButton();
 void usedTime();
+void changableString (char* time_string_p, int* timep, int* temp_list);
+char changeText(char* edit_character, int* temp_list, int character_number);
+void checkPage100ms();
+void allowCheckPage100ms();
+void cleaningTimesFun();
+int switch2Page3();
--- MotionSensorfunctions.c
+++ MotionSensorfunctions.c
@@ -20,24 +20,106 @@ int counter =0;
 int port_turn_off_check =0;
 int page_check=0;
 
+//variables for extra functions (editabele text)
+char editable_text_busy[16];
+char editable_text_empty[16];
+int secondary_list_busy[16];
+int secondary_list_empty[16];
+int char_value = 64; // char (A)
+int display_length = 0;
+int character_number_busy = 0;
+int character_number_empty = 0;
+int check=0;
+int init=1;
+
+//variables for extra functions(cleaning times)
+char cleaningTimes[20][12];
+int buttonReleased = 0;
+int timesCleaned = 0;
+int displayCleanedIndex = 0;
+
+int displayCleanedIndexReleased = 0;
+
 /* Interrupt Service Routine */
 void user_isr( void ) // calls repeatdly in a specific time
 {
 	if (IFS(0) & 0x100)		//will enter the if-statment every 100ms 
 	{
-		counter_for_timeout++;	
-		
+		counter_for_timeout++;
+		allowCheckPage100ms(); // to call the string every 100 ms
 		if (counter_for_timeout>=10)	//
 		{
-			
 			checkPage();
 			timeCounter();
 			counter_for_timeout =0;
 			usedTime();
 		}
-		
 		IFSCLR(0)=0x100;
 	}
+}
+void allowCheckPage100ms()// 
+{
+	int get_switches = getsw();	// to call the function only in the first page
+	if (get_switches & 0x2){
+		
+	}
+	else if(get_switches & 0x1) {
+		checkPage();
+	}
+	else {
+		checkPage100ms();
+	}
+}
+
+void setupText()
+{
+	editable_text_empty[0]='E';
+	editable_text_empty[1]='M';
+	editable_text_empty[2]='P';
+	editable_text_empty[3]='T';
+	editable_text_empty[4]='Y';
+	editable_text_empty[5]=0x00;
+	
+	editable_text_busy[0]='B';
+	editable_text_busy[1]='U';
+	editable_text_busy[2]='S';
+	editable_text_busy[3]='Y';
+	editable_text_busy[4]=0x00;
+	
+}
+
+void checkPage100ms(){ // call the changeText function every 100 ms.
+	if(init == 1)
+	{
+		setupText();
+		init=0;
+	}
+	
+	if (PORTE == 0){
+		//changeText(editable_text_empty, secondary_list_empty, character_number_empty); // calling function with passing parameters (empty)
+		changeText(editable_text_empty, secondary_list_empty, character_number_empty);
+		display_string(1, editable_text_empty);
+		//display_string( 1,editable_text_empty);
+		display_update();
+		if (!check) // to let the character_number start from the begining once it changed the status and for switching for the second one
+		{
+			check=1;
+			display_length=0; 
+		}
+	}
+	
+	 if (PORTE == 1){ // calling function for busy situation when PORTE =1 (busy) 
+		changeText(editable_text_busy, secondary_list_busy, character_number_busy);
+		display_string( 1,editable_text_busy);
+		display_update();
+		if (!check) // to let the character_number start from the begining once it changed the status and for switching for the second one
+		{
+			check=1;
+			display_length=0;
+		}
+	}
+	
+	
 }
 void usedTime() // the time the toilet has been used
 {
@@ -67,20 +149,20 @@ void usedTime() // the time the toilet has been used
 }
 int checkPage () // check which slides and button are on and if there is none, choose default
 {	
-int butns = getBtns();			//get buttons values
-int get_switches = getSw();		//get switch value
-	if (get_switches & 0x2) // switch 2
-	{
-		switch2Page2();		//call switch2Page2
-	}
-	else if ((get_switches & 0x8) && (butns & 0x8)) // reset when button 4 is pushed and switch slide is on 
-	{
-		resetButton();		
-	}
-	else 
-	{
-		switch2Page1();
-	}
+	//int butns = getBtns();			//get buttons values
+	int get_switches = getsw();		//get switch value
+		if (get_switches & 0x2) // switch 2
+		{
+			switch2Page2();		//call switch2Page2
+		}
+		else if (get_switches & 0x1)
+		{
+			switch2Page3();
+		}
+		else 
+		{
+			switch2Page1();
+		}
 }
 void toStringDisplay (char* time_string_p, int* timep, char between) // convert time to string to show it on display
 {
@@ -111,12 +193,13 @@ int switch2Page1() // what to do in page 1
 	display_update();
 	motionSensorOn();	//get the information from motionSensorOn
 	display_string( 0,"PAGE 1" );
+	checkPage100ms();
 	display_string(3,time_string);
 	display_string( 2,"" );
 	display_update();
 	motion_sens = PORTB & 0x2;	//
 	toStringDisplay(time_string, time, ':');
-	
+	cleaningTimesFun();
 	
 }
 
@@ -128,29 +211,149 @@ int switch2Page2()// what to do in page 2
 	display_string( 3,"" );		//line 4
 	display_update();			//update the screen every time we update the information
 	display_string( 0,"PAGE 2" );
-	display_string( 1, "USAGE AVERAGE" );
+	display_string( 1, "AVERAGE USAGE" );
 	display_string(2, "<2 <4 <6 6<");
 	display_string(3, used_time_string);	//disable the information from used_time_string
 	display_update();
 	toStringDisplay(used_time_string, range_used_time, ' ');	//
 }
-int resetButton() // what to do in reset page 
+
+void changeDisplayIndex()
 {
-	int i;
-	for (i =0; i<4; i++)	//
-	{
-		time[i] =00;
-		range_used_time[i]=00;	//set the whole array to 0
-	}
-	display_string( 0,"" );
-	display_string(1,"" );
-	display_string( 2,"" );
-	display_string( 3,"" );		//empty the four lines 
-	display_update();
-	display_string(0,"RESET" );
-	display_string(3,time_string);
-	display_update();
+	int butns = getbtns();
 	
+	
+    if ((butns & 0x2) && (displayCleanedIndexReleased == 1))
+    {
+        displayCleanedIndexReleased = 0;
+		displayCleanedIndex++;
+		
+    }
+	
+	 if ((butns & 0x1) && (displayCleanedIndexReleased == 1))
+    {
+	
+        displayCleanedIndexReleased = 0;
+		displayCleanedIndex--;
+		if(displayCleanedIndex <=0) displayCleanedIndex = 0;
+		
+    }
+    if ((butns & 0xf) == 0)
+    {
+        displayCleanedIndexReleased = 1;
+    }
+}
+
+
+
+int switch2Page3()// what to do in page 2
+{
+	display_string( 0,"" );
+	display_string( 1,"" );
+	display_string( 2,"" );
+	display_string( 3,"" );
+	display_update();
+	//motionSensorOn();	//get the information from motionSensorOn
+	
+	//checkPage100ms();
+	//display_string(3,time_string);
+	//display_string( 2,cleaningTimes[i][4]);
+	display_string( 0,"PAGE 3" );
+	cleaningTimesFun();
+	changeDisplayIndex();
+		display_string(1,cleaningTimes[displayCleanedIndex]);
+	
+display_string(2,cleaningTimes[displayCleanedIndex+1]);
+	
+	
+
+display_string(3,cleaningTimes[displayCleanedIndex+2]);
+	
+
+
+	
+	display_update();
+}
+
+
+char changeText(char* edit_character, int* temp_list, int character_number) // what to do in textChanging page instead of busy and available
+{
+	int butns = getbtns();	
+	int get_switches = getsw();	
+	if (get_switches & 0x8) // key for start editing the shown text on display
+	{
+		if (butns & 0x4) // increase the value of the character in ascii table
+		{
+			char_value++;
+			temp_list[display_length] = char_value;
+			changableString (edit_character, &char_value, temp_list); // call the fun and change in variables (char_value) value.
+			//display_string( 1,edit_character );
+			//display_update();
+		}
+		else if (butns & 0x2) // decrease the value of the char in ascii table
+		{
+			char_value--;
+			temp_list[display_length] = char_value;
+			changableString (edit_character, &char_value, temp_list);
+			//display_string( 1,edit_character );
+			//display_update();
+		}
+		else if (butns & 0x1) // jump to the next char in length
+		/* increase the index of character and increase charcter_number variable value to declare it as the number of characters in line 
+		use secondary list to save the recent value of character to reuse in when jumping to the next or previous character index in string*/
+		{
+			if (display_length >= 15){
+			display_length = 0;
+			}
+			else if (display_length<=14){
+				display_length++;
+				character_number++; // while moving to next character // save the length of text to jump to it while going to the left side when display_length is =0;
+			}
+			char_value = temp_list[display_length]; // save value of character values into a temporary list to save them and not reuse them in second character array.
+			changableString (edit_character, &char_value, temp_list); 
+			//display_string( 1,edit_character );
+			//display_update();
+		}
+		else if (butns & 0x8) // jump to the first previous char in length
+		/* decreasing the display_length decide which index will be changed and presented */
+		{
+			if (display_length>0){
+				display_length--;
+			}
+			else if (display_length <= 0){
+				display_length = character_number;
+			}
+			char_value = temp_list[display_length];
+			changableString (edit_character, &char_value, temp_list);
+			//display_string( 1,edit_character );
+			//display_update();
+		}
+	}
+	else {display_length=0;} // to keep the default value of display_length;
+}
+void changableString (char* time_string_p, int* timep, int* temp_list) // convert time to string to show it on display
+{
+	if (*timep == 91 || *timep == 64){
+		*timep = 32;
+		time_string_p[display_length] = *timep;
+		*timep = 91;
+	}
+	else if (*timep > 90)
+	{
+		*timep = 65;
+		time_string_p[display_length] = *timep;
+	}
+	else if (*timep < 65)
+	{
+		*timep=90;
+		time_string_p[display_length] = *timep;
+	}
+	else if (*timep<=90)
+	{
+		time_string_p[display_length] = *timep;
+	}
+	temp_list[display_length] = time_string_p[display_length] ;
+	time_string_p[16] = 0;
 }
 int motionSensorOn() // when the sensor is on
 {
@@ -158,26 +361,14 @@ int motionSensorOn() // when the sensor is on
 	if (motion_sens && PORTE==0)	//when the sens is 1 and the port(led) is 0, then set the port(led) to 1 and display "Busy" on the screen.
 	{
 		PORTE = 1;
-		display_string(1,"BUSY");
-		display_update();
+		check = 0;
 		return 1;
 	}
 	else if (motion_sens && PORTE ==1)	//when the sens is 1 and the port(led) is 1, then set the port(led) to 0 and display "AVAILABLE" on the screen.
 	{
 		PORTE = 0;
-		display_string(1,"AVAILABLE");
-		display_update();
+		check =0;
 		return 0;
-	}
-	else if (PORTE==1)		//display Busy when we turn on the ChipKit for first time when the port is on.
-	{
-		display_string(1,"BUSY");
-		display_update();
-	}
-	else if (PORTE==0)		//display AVAILABLE when we turn on the shipKit for first time when the port is off.
-	{
-		display_string(1,"AVAILABLE");
-		display_update();
 	}
 }
 int timeCounter() // count actual time
@@ -200,6 +391,65 @@ int timeCounter() // count actual time
 	}
 }
 
+//Karoline Extra Function (show last 20 times the bathroom has be cleaned)
+
+
+void shiftCleaningTimes()
+{
+	int i=0;
+	int j=0;
+	for(i=0; i < 19; i++)
+	{
+		for(j=0; j < 11; j++)
+		{
+			cleaningTimes[i][j] = cleaningTimes[i+1][j];
+		}
+		cleaningTimes[i][11] = 0x00;
+	}
+	
+}
+
+void copyMemTime(int index)
+{
+	int i=0;
+	for(i=0;i<11;i++)
+	{
+		cleaningTimes[index][i]=time_string[i];
+	}
+	cleaningTimes[index][11] = 0x00;
+}
+
+void addCleaningTime(int index)
+{
+	
+	if(index >= 20)
+	{
+		shiftCleaningTimes();
+		copyMemTime(19);
+	}
+	else
+	{
+		copyMemTime(index);
+	}
+	
+}
+
+
+void cleaningTimesFun()//(char* time_Stringpara)
+{
+    int butns = getbtns();
+    if ((butns & 0x4) && (buttonReleased == 1))
+    {
+        buttonReleased = 0;
+		addCleaningTime(timesCleaned);
+        timesCleaned++;
+    }
+    else if ((butns & 0x4) == 0)
+    {
+        buttonReleased = 1;
+    }
+}
+
 void labinit( void ) // declare PORTS specifications
 {
 	TRISD |= 0xfe0;
@@ -220,18 +470,6 @@ void labinit( void ) // declare PORTS specifications
 	IECSET(0) = 0x100; // Enable timer 2 interrupts
 	IPCSET(2) = 0xF; // Set priority for timer 2 interrupts
 	enable_interrupt(); // Enable interrupts globally
-}
-int getSw() // get switches values
-{
-	int shiftPORTD = PORTD >> 8; // switches are connected to bits 8 - 11
-	shiftPORTD = shiftPORTD & 0xf; // last digit (4 bits)
-	return shiftPORTD;
-}
-int getBtns() // get buttons values
-{
-	int shiftPORTD = PORTD >> 4;// buttons are connected to bits 4,5,6 and 7
-	shiftPORTD = shiftPORTD & 0xf;// comparing between 4 bits which has the sum 15
-	return shiftPORTD;
 }
 void labwork( void ) // enable 
 {
